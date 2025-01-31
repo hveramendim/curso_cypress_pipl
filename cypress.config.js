299ef7858a516e92716de046041dcf2ccfbd9bc5
hveramendi <henry.veramendi.rp@viaexperis2.pe>
2025-01-31 15:10:19 -0500

    tired of this shit x10

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,15 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Configuración de eventos
+      // Configuración adicional de eventos si es necesario
       return config;
     },
-    //baseUrl: 'http://localhost:3000',  // Asegúrate de que el servidor esté disponible si necesitas pruebas E2E
+    baseUrl: 'http://localhost:3000',  // Asegúrate de que el servidor esté levantado
     video: true,
     retries: {
       runMode: 2,
       openMode: 1,
     },
   },
+  projectId: 'uvpmbq',  // Reemplaza con el ID correcto de tu proyecto en Cypress Dashboard
 });
