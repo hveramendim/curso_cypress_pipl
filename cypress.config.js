48455e86a9bfd587f790a78b03d8ae829a00e154
hveramendi <henry.veramendi.rp@viaexperis2.pe>
2025-01-31 14:58:41 -0500

    tired of this shit x8

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,11 @@
-const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Aquí puedes agregar otros eventos si los necesitas
-      on('before:browser:launch', (browser = {}, launchOptions) => {
-        console.log(`Launching browser: ${browser.name}`);
-      });
-
-      return config;
+      // Configuración de eventos
     },
-    baseUrl: 'http://localhost:3000',  // Ajusta esta URL según tu aplicación
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',  // Asegura que Cypress lea los archivos de prueba correctamente
-    supportFile: 'cypress/support/e2e.js',
-    video: true,         // Grabar videos (puedes desactivar esta opción)
-    retries: {
-      runMode: 2,        // Reintenta pruebas fallidas en ejecución normal
-      openMode: 1,       // Reintenta pruebas fallidas en modo abierto (GUI)
-    },
+    // Elimina esta línea si no necesitas levantar un servidor
+    // baseUrl: 'http://localhost:3000',
+    video: true,
+    retries: { runMode: 2, openMode: 1 },
   },
-  projectId: 'uvpmbq',
 });
